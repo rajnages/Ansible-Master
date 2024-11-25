b64bbafe154d8c809c56b84f1aea28e2f30ef5ad
Rajavarapu Nageswarao <nagesrajavarapu@gmail.com>
2024-11-25 10:44:00 +0530

    bucket is added

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,7 +11,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "workspacesbucket-ansible"
+    bucket         = "workspacesbucket-ansible-01"
     key            = "Ansible.tfstate"
     region         = "us-east-1"
     # dynamodb_table = "-terraform-locks"
